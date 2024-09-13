c46eb225f90b97cfe1a6ef67e6b52b5cface1017
sibeni-li <pro@sibeni.li>
2024-09-14 01:01:29 +0200

    Initialize server

--- server.js
+++ server.js
@@ -1,70 +1,55 @@
-const express = require('express');
-const path = require('path');
-const Mailjet = require('node-mailjet');
+const http = require('http');
+const app = require('./app');
 
-const app = express();
-const port = 3000;
 
-console.log(process.env);
-const mailjet = Mailjet.apiConnect(
-    process.env.MJ_APIKEY_PUBLIC,
-    process.env.MJ_APIKEY_PRIVATE,
-    {
-      config: {},
-      options: {}
-    } 
-);
-// Middleware pour parser les données JSON
-app.use(express.json());
+// Function to normalize the port number
+const normalizePort = val => {
+  const port = parseInt(val, 10);
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+// Set the port for the server
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+// Error handler for server errors
+const errorHandler = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+// Create HTTP server
+const server = http.createServer(app);
+
+
+// Set up error and listening event handlers
+server.on('error', errorHandler);
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
 });
 
-app.use(express.static(path.join(__dirname, '../frontend')));
-
-// Route pour gérer la soumission du formulaire
-app.post('/send-email', (req, res) => {
-    const { name, nameSociety, email, message } = req.body;
-
-    if (!name || !email || !message) {
-        return res.status(400).send('Données manquantes');
-    }
-
-    // Configurer l'email à envoyer
-    const request = mailjet.post('send', { version: 'v3.1' }).request({
-        Messages: [
-            {
-                From: {
-                    Email: "contact-portfolio@sibeni.li",
-                    Name: `${name}`,
-                },
-                To: [
-                    {
-                        Email: "contact@sibeni.li",
-                        Name: "Lisa Sibeni",
-                    },
-                ],
-                Subject: `Nouveau message de ${name}`,
-                TextPart: `Nom: ${name}\nSociété: ${nameSociety}\nEmail: ${email}\nMessage: ${message}`,
-            },
-        ],
-    });
-    // Envoyer l'email
-    request
-    .then((result) => {
-        console.log(result.body);
-        res.status(200).send('Email envoyé avec succès!');
-    })
-        .catch((err) => {
-            console.error(err.statusCode);
-            res.status(500).send('Erreur lors de l\'envoi de l\'email.');
-        });
-});
-
-app.listen(port, () => {
-    console.log(`Serveur démarré sur le port ${port}`);
-});
+// Start the server
+server.listen(port);
